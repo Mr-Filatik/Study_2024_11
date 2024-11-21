ee2ea1d35f3c4560b6640b8954a17ce6f6bf6d55
Mr-Filatik <filatov23wlad@gmail.com>
2024-11-21 20:11:51 +0400

    Adding DataGrid

--- Applications/Desktop/Applications.Desktop.AdminPanel/UsersWindow.xaml.cs
+++ Applications/Desktop/Applications.Desktop.AdminPanel/UsersWindow.xaml.cs
@@ -8,25 +8,59 @@ namespace Applications.Desktop.AdminPanel
     /// </summary>
     public partial class UsersWindow : Window
     {
-        User user = new User()
+        //User user = new User()
+        //{
+        //    Id = 1,
+        //    FirstName = "Test",
+        //    SecondName = "TestTest",
+        //    Age = 20,
+        //    Gender = null
+        //};
+
+        User[] users = new User[3]
         {
-            Id = 1,
-            FirstName = "Test",
-            SecondName = "TestTest",
-            Age = 20,
-            Gender = null
+            new User()
+            {
+                Id = 1,
+                FirstName = "Test1",
+                SecondName = "TestTest1",
+                Age = 20,
+                Gender = null
+            },
+            new User()
+            {
+                Id = 2,
+                FirstName = "Test2",
+                SecondName = "TestTest2",
+                Age = 22,
+                Gender = true
+            },
+            new User()
+            {
+                Id = 3,
+                FirstName = "Test3",
+                SecondName = "TestTest3",
+                Age = 24,
+                Gender = false
+            },
         };
 
         public UsersWindow()
         {
             InitializeComponent();
 
-            this.DataContext = user;
+            usersDataGrid.ItemsSource = users;
+            //this.DataContext = user;
         }
 
         private void GetUserDataClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"Id:{user.Id},FirstName:{user.FirstName},SecondName:{user.SecondName}.");
+            var user = usersDataGrid.SelectedItem as User;
+            if (user != null)
+            {
+                MessageBox.Show($"Id:{user.Id},FirstName:{user.FirstName},SecondName:{user.SecondName}.");
+            }
+            //MessageBox.Show($"Id:{user.Id},FirstName:{user.FirstName},SecondName:{user.SecondName}.");
         }
     }
 }
