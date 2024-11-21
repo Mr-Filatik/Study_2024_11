e353473acae33e5c969be848ddad317b8ee91251
Mr-Filatik <filatov23wlad@gmail.com>
2024-11-21 19:33:20 +0400

    Adding binding in admin project

---
+++ Applications/Desktop/Applications.Desktop.AdminPanel/UsersWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Study_2024_11.Shared.Entities;
+using System.Windows;
+
+namespace Applications.Desktop.AdminPanel
+{
+    /// <summary>
+    /// Interaction logic for UsersWindow.xaml
+    /// </summary>
+    public partial class UsersWindow : Window
+    {
+        User user = new User()
+        {
+            Id = 1,
+            FirstName = "Test",
+            SecondName = "TestTest",
+            Age = 20,
+            Gender = null
+        };
+
+        public UsersWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = user;
+        }
+
+        private void GetUserDataClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Id:{user.Id},FirstName:{user.FirstName},SecondName:{user.SecondName}.");
+        }
+    }
+}
