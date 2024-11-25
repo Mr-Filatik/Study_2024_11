551a562878eaefb8abeb279d5d4a1779f2781886
Mr-Filatik <filatov23wlad@gmail.com>
2024-11-25 19:04:25 +0400

    Adding LoginViewModel

---
+++ Applications/Desktop/Applications.Desktop.AdminPanel/ViewModels/LoginViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applications.Desktop.AdminPanel.ViewModels;
+
+public class LoginViewModel : INotifyPropertyChanged
+{
+    #region Properties
+
+    private string _loginField;
+    public string LoginField
+    {
+        get
+        {
+            return _loginField;
+        }
+        set
+        {
+            _loginField = value;
+            OnPropertyChanged(nameof(LoginField));
+        }
+    }
+
+    private string _passwordField;
+    public string PasswordField
+    {
+        get
+        {
+            return _passwordField;
+        }
+        set
+        {
+            _passwordField = value;
+            OnPropertyChanged(nameof(PasswordField));
+        }
+    }
+
+    #endregion
+
+    #region Implement INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged(string name = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    #endregion
+}
