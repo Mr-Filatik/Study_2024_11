cdd828b93d575115d2b732a8635ee45d69ce2025
Mr-Filatik <filatov23wlad@gmail.com>
2024-11-25 19:08:11 +0400

    Add mvvm

--- Applications/Desktop/Applications.Desktop.AdminPanel/App.xaml.cs
+++ Applications/Desktop/Applications.Desktop.AdminPanel/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Applications.Desktop.AdminPanel.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,11 @@ namespace Applications.Desktop.AdminPanel
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            LoginWindow window = new LoginWindow();
+            window.DataContext = new LoginViewModel();
+            window.Show();
+        }
     }
-
 }
