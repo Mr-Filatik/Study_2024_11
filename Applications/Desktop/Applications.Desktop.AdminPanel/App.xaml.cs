b469ab654f31b336a102b9414eadb02122c4b0c3
Mr-Filatik <filatov23wlad@gmail.com>
2024-11-26 19:37:16 +0400

    Add DI

--- Applications/Desktop/Applications.Desktop.AdminPanel/App.xaml.cs
+++ Applications/Desktop/Applications.Desktop.AdminPanel/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Applications.Desktop.AdminPanel.ViewModels;
+﻿using Applications.Desktop.AdminPanel.Configuration;
+using Applications.Desktop.AdminPanel.ViewModels;
+using Applications.Desktop.AdminPanel.ViewModels.Abstract;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace Applications.Desktop.AdminPanel
@@ -8,13 +11,32 @@ namespace Applications.Desktop.AdminPanel
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            //LoginNewWindow window = new LoginNewWindow(new LoginViewModel());
-            //LoginWindow window = new LoginWindow(new LoginViewModel());
-            LoginWindow window = new LoginWindow(new LoginNewViewModel());
+            var services = new ServiceCollection();
+
+            services.RegisterViews();
+            services.RegisterViewModels();
+
+            services.AddSingleton<ApplicationUser>();
+
+            _serviceProvider = services.BuildServiceProvider();
+
+            var window = _serviceProvider.GetRequiredService<LoginWindow>();
             window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             window.Show();
         }
+
+        public void OpenUserWindow()
+        {
+            _serviceProvider.GetRequiredService<UsersWindow>().Show();
+        }
+    }
+
+    public class ApplicationUser
+    {
+        public string Name { get; set; } = Guid.NewGuid().ToString();
     }
 }
--- Applications/Desktop/Applications.Desktop.AdminPanel/LoginWindow.xaml.cs
+++ Applications/Desktop/Applications.Desktop.AdminPanel/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Applications.Desktop.AdminPanel.ViewModels.Abstract;
+﻿using Applications.Desktop.AdminPanel.ViewModels;
+using Applications.Desktop.AdminPanel.ViewModels.Abstract;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -10,12 +11,13 @@ namespace Applications.Desktop.AdminPanel
     /// </summary>
     public partial class LoginWindow : Window
     {
-        public LoginWindow(ILoginViewModel viewModel)
+        public LoginWindow(ILoginViewModel viewModel, ApplicationUser user)
         {
             InitializeComponent();
 
             DataContext = viewModel;
             viewModel.MessageBoxYesNo += ShowMessageBoxYesNo;
+            //ShowMessageBoxYesNo(user.Name, "User");
         }
 
         private bool ShowMessageBoxYesNo(string message, string title)
