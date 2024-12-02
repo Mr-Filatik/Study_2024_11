f911f8946366a11ee15bded3ffddfd2492b7fab0
Mr-Filatik <filatov23wlad@gmail.com>
2024-12-02 20:39:30 +0400

    Add EF ORM to Sqlite

---
+++ Shared/Database/Shared.Database.MainDatabase/MainDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Study_2024_11.Shared.Entities;
+
+namespace Shared.Database.MainDatabase;
+
+public class MainDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public MainDbContext()
+    {
+        //Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=main_db;Username=main_user;Password=main_password");
+        optionsBuilder.UseSqlite("Data Source=test_db_study_2024_11.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(new User()
+        {
+            Id = 1,
+            FirstName = "Admin",
+            SecondName = "Main",
+            Age = 100,
+            Email = "admin@mail.com",
+            PasswordHash = "password",
+            Gender = true,
+        });
+    }
+}
