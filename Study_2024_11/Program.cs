f07cbc8c5feac224141b731bc29134e2d3a4839e
Mr-Filatik <filatov23wlad@gmail.com>
2024-12-09 20:45:04 +0400

    Some changes

--- Study_2024_11/Program.cs
+++ Study_2024_11/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Study_2024_11;
+﻿using System.Text;
+
+namespace Study_2024_11;
 
 public class Program
 {
@@ -6,15 +8,22 @@ public class Program
     {
         Console.WriteLine("Hello, World!");
 
+        var str = "csjbcsjs";
+        Console.WriteLine(str.RemoveModTwo());
+
         var storage = new MemoryStorage();
 
-        storage.SetValue("SERVER_ADDRESS", "localhost:8080");
+        //storage.SetValue("SERVER_ADDRESS", "localhost:8080");
 
-        SellerPanel(storage);
+        //SellerPanel(storage);
 
-        UserPanel(storage);
+        //UserPanel(storage);
 
-        AdminPanel(storage);
+        //AdminPanel(storage);
+
+        //var repository = new EntityRepository<User>();
+        //repository.Create(new User());
+        //repository.Create(new Admin());
     }
 
     public static void UserPanel(IStorage storage)
@@ -40,5 +49,21 @@ public class Program
         storage.SetValue("PRODUCT_ID", "152278767");
 
         Console.WriteLine(storage.GetValue("PRODUCT_ID"));
+    }
+}
+
+public static class StringExtension
+{
+    public static string RemoveModTwo(this string str)
+    {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                res.Append(str[i]);
+            }
+        }
+        return res.ToString();
     }
 }
