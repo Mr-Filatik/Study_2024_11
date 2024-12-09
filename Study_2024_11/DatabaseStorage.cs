501269a310fa017cf42d951ee1b9f6dd5d313236
Mr-Filatik <filatov23wlad@gmail.com>
2024-12-09 19:31:31 +0400

    Interface logic

---
+++ Study_2024_11/DatabaseStorage.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Study_2024_11;
+
+public class DatabaseStorage : IStorage
+{
+    private Dictionary<string, string> _memory = new Dictionary<string, string>();
+
+    public void SetValue(string name, string value)
+    {
+        // database
+        if (_memory.ContainsKey(name))
+        {
+            _memory[name] = value;
+        }
+        _memory.Add(name, value);
+    }
+
+    public string? GetValue(string name)
+    {
+        // database
+        if (_memory.ContainsKey(name))
+        {
+            return _memory[name];
+        }
+        return null;
+    }
+}
--- Study_2024_11/Program.cs
+++ Study_2024_11/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Study_2024_11;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var storage = new MemoryStorage();
+
+        storage.SetValue("SERVER_ADDRESS", "localhost:8080");
+
+        SellerPanel(storage);
+
+        UserPanel(storage);
+
+        AdminPanel(storage);
+    }
+
+    public static void UserPanel(IStorage storage)
+    {
+        // преобразования (плохой вариант) может выдать ошибку при null
+        //var stor1 = (MemoryStorage)storage;
+        // преобразования (хороший вариант) вернёт null если не сможет преобразовать
+        var stor = storage as MemoryStorage;
+        if (stor != null)
+        {
+            stor.SetString();
+        }
+        Console.WriteLine(storage.GetValue("PRODUCT_ID"));
+    }
+
+    public static void AdminPanel(IStorage storage)
+    {
+        Console.WriteLine(storage.GetValue("SERVER_ADDRESS"));
+    }
+
+    public static void SellerPanel(IStorage storage)
+    {
+        storage.SetValue("PRODUCT_ID", "152278767");
+
+        Console.WriteLine(storage.GetValue("PRODUCT_ID"));
+    }
+}
