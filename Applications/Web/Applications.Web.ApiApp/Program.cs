1571d411423af7920937ea0c9c4f14766a1e438c
Mr-Filatik <filatov23wlad@gmail.com>
2024-12-17 20:25:46 +0400

    Adding auth in api

--- Applications/Web/Applications.Web.ApiApp/Program.cs
+++ Applications/Web/Applications.Web.ApiApp/Program.cs
@@ -1,5 +1,10 @@
 using Swashbuckle.AspNetCore.Filters;
 using Shared.Database.MainDatabase.Configuration;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Applications.Web.ApiApp
 {
@@ -13,12 +18,57 @@ namespace Applications.Web.ApiApp
 
             builder.Services.AddMainDatabase();
 
+            builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"])),
+                    };
+                });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                var b = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme);
+                b = b.RequireAuthenticatedUser(); //RequireRole or RequireClaims
+                options.DefaultPolicy = b.Build();
+            });
+
             builder.Services.AddControllers();
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
             {
-                //
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token.",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer",
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[]{}
+                    }
+                });
                 options.ExampleFilters();
             });
 
@@ -37,6 +87,7 @@ namespace Applications.Web.ApiApp
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.MapControllers();
 
